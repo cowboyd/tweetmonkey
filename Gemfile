1c18f47753e906e690cdaac5caf65dbfbe2609d9
Charles Lowell <cowboyd@thefrontside.net>
2009-11-09 09:31:28 -0500

    plugin a rudimentary twitter interface

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 disable_system_gems
 
 gem "sinatra"
+gem "twitter"
 gem "json"
+gem "jruby-openssl"
 gem "therubyrhino", :git => "git@github.com:cowboyd/therubyrhino.git"
 
--- tweetmonkey.rb
+++ tweetmonkey.rb
@@ -1,6 +1,7 @@
 require File.dirname(__FILE__) + "/vendor/gems/environment"
 require 'sinatra'
 require 'JSON'
+require 'twitter'
 require 'rhino'
 
 get "/" do
@@ -8,7 +9,49 @@ get "/" do
 end
 
 post "/eval" do
-  Rhino::Context.open_std do |cxt, scope|
-    JSON.pretty_generate(cxt.eval(params[:source] || '', scope))
+    Rhino::Context.open_std do |cxt, scope|
+      scope['monkey'] = TweetMonkey.new(params[:username], params[:password])
+      JSON.pretty_generate begin
+        cxt.eval(params[:source], scope)
+      rescue StandardError => e
+        {:error => e.message}
+      end
+    end
+end
+
+class TweetMonkey
+  def initialize(u, p)
+    Twitter::HTTPAuth.new(u,p).tap do |auth|
+      @client = Twitter::Base.new(auth)
+    end
+  end
+
+  def friends
+    @client.friends
+  end
+
+  def followers
+    @client.followers
+  end
+
+  def friends_timeline
+    @client.friends_timeline
+  end
+
+  def user_timeline
+    @client.user_timeline
+  end
+
+  def replies
+    @client.replies
+  end
+
+  def search(query = nil, options = {})
+
+    Twitter::Search.new(query).tap do |s|
+      for field,value in options
+        s.send(field, value) if s.respond_to?(field)
+      end
+    end.fetch
   end
 end
